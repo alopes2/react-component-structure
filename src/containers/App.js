fb61cdd33affb0a4ee22eb3748ce8041ef0c1a00
Andre Lopes <andrevitorlopes@gmail.com>
2018-01-13 21:04:55 -0200

    using pure components to validate if the component should or not update automatically

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import classes from  './App.css';
 // import Radium, { StyleRoot } from 'radium';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
-class App extends Component {
+class App extends PureComponent {
   constructor(props) {
     super(props);
     console.log('[App.js Inside Constructor: ', props);
@@ -26,10 +26,11 @@ class App extends Component {
     console.log('[App.js] Inside componentDidMount');
   }
 
-  shouldComponentUpdate (nextProps, nextState) {
-      console.log('[UDPATE App.js] Inside shouldComponentUpdate', nextProps, nextState);
-      return true;
-  }
+  // shouldComponentUpdate (nextProps, nextState) {
+  //     console.log('[UDPATE App.js] Inside shouldComponentUpdate', nextProps, nextState);
+  //     return nextState.persons !== this.state.persons ||
+  //       nextState.showPersons !== this.state.showPersons;
+  // }
 
   componentWillUpdate (nextProps, nextState) {
       console.log('[UDPATE App.js] Inside componentWillUpdate', nextProps, nextState);
@@ -93,6 +94,7 @@ class App extends Component {
     return (
       /* <StyleRoot> */
         <div className={classes.App}>
+          <button onClick={() => {this.setState({showPersons: true})}}>Show Persons</button>
           <Cockpit 
             appTitle={this.props.title}
             showPersons={this.state.showPersons}
