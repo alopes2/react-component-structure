5b20bfe5024cf8b88ab4e2786aba88518e4faf1f
Andre Lopes <andrevitorlopes@gmail.com>
2018-01-13 19:16:47 -0200

    turning stateless into stateful components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { Component} from 'react';
 import classes from './Person.css';
 // import Radium from 'radium';
 
-const person = (props) => {
+class Person extends Component {
     // When using class-based components, it`s this.props
     // like <p>My name is {this.props.name}</p>
     // const style = {
@@ -10,15 +10,17 @@ const person = (props) => {
     //         width: '450px'
     //     }
     // }
-    return (
-        /* <div className="Person" style={style}> */
-        <div className={classes.Person}>
-            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.change} value={props.name}/>
-        </div>
-    );
-};
+    render() {
+        return (
+            /* <div className="Person" style={style}> */
+            <div className={classes.Person}>
+                <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
+                <p>{this.props.children}</p>
+                <input type="text" onChange={this.props.change} value={this.props.name}/>
+            </div>
+        );
+    }
+}
 
-export default person;
-// export default Radium(person);
+export default Person;
+// export default Radium(Person);
