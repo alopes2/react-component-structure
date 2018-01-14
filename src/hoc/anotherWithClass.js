f514ccf8049a735e1f100bfe7f3f93f955c5d336
Andre Lopes <andrevitorlopes@gmail.com>
2018-01-14 12:35:30 -0200

    turning into stateful  component wrapper

--- src/hoc/anotherWithClass.js
+++ src/hoc/anotherWithClass.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+// const anotherWithClass = (WrappedContainer, className) => {
+//     return (props) => (
+//         <div className={className}>
+//             <WrappedContainer {...props}/>
+//         </div>
+//     )
+// };
 
 const anotherWithClass = (WrappedContainer, className) => {
-    return (props) => (
-        <div className={className}>
-            <WrappedContainer {...props}/>
-        </div>
-    )
+    return class extends Component {
+        render() {
+            return (
+                <div className={className}>
+                    <WrappedContainer {...this.props}/>
+                </div>
+            );
+        }
+    }
 };
 
 export default anotherWithClass;
